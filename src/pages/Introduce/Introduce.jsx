37f8259be5219c2a753bbeb9828600b6c0e6f562
SangHyeon <sanghyeon@beamworks.co.kr>
2023-11-06 15:55:33 +0900

    슬로건 컴포넌트 개발

---
+++ src/pages/Introduce/Introduce.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Slogun from "./component/Slogun";
+import "./styles/introduce.css";
+
+const Introduce = () => {
+  return <Slogun />;
+};
+
+export default Introduce;
