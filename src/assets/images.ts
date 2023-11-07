4acfc7904705e45b718cf4663ce1f86427cc86d8
SangHyeon <sanghyeon@beamworks.co.kr>
2023-11-07 14:34:06 +0900

    빔웍스의 미션 컴포넌트 개발

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -8,6 +8,10 @@ const images = {
 	language: '/assets/images/language.svg',
 	mainback1: '/assets/images/mainback1.png',
 	goal_description: '/assets/images/goal_description.png',
+	mission_image: '/assets/images/mission_image.png',
+	mission_detail1: '/assets/images/mission_detail1.png',
+	mission_detail2: '/assets/images/mission_detail2.png',
+	mission_detail3: '/assets/images/mission_detail3.png',
 };
 
 export default images;
--- src/pages/Introduce/component/IntroduceMission.jsx
+++ src/pages/Introduce/component/IntroduceMission.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import images from "src/assets/images";
+import "../styles/introduce.css";
+
 const IntroduceMission = () => {
-  return <div></div>;
+  return (
+    <section className="IntroduceMission">
+      <div className="Mission">
+        <div className="MissionTitle">
+          <label>빔웍스의 미션</label>
+        </div>
+        <div className="MissionSubTitle">
+          <label>서로를 위한 빔웍스</label>
+        </div>
+        <div className="MissionDescription">
+          <label>
+            빔웍스는 직원을 성장을 위해 최선을 다하며
+            <br />
+            직원은 환자와 의료진을 위해 끊임없이 연구합니다.
+          </label>
+        </div>
+        <img className="MissionImage" src={images.mission_image}/>
+      </div>
+    </section>
+  );
 };
 
 export default IntroduceMission;
