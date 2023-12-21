a4932b4479dc3764754b4d0387c5d11070a944a5
Noah <seohyun060@naver.com>
2023-12-21 15:09:22 +0900

    임시

---
+++ src/pages/News/News.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/news.styles.css';
+type Props = {};
+
+const News = (props: Props) => {
+	return <div className='news'>News</div>;
+};
+
+export default News;
---
+++ src/pages/News/containers/NewsContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import News from '../News';
+type Props = {};
+
+const NewsContainer = (props: Props) => {
+	return <News />;
+};
+
+export default NewsContainer;
