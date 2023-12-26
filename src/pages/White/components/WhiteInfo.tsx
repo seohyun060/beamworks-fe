bb7c6c05ca7d46d2bda3bafe552f2c9d2f0a8ad5
Noah <seohyun060@naver.com>
2023-12-26 10:30:10 +0900

    white 페이지 퍼블리싱

---
+++ src/pages/White/components/WhiteInfo.tsx
@@ -0,0 +1,29 @@
+import { PubInfo } from '@typedef/types';
+import images from 'src/assets/images';
+import React from 'react';
+
+type Props = {
+	white: PubInfo;
+};
+
+const whitelicationInfo = ({ white }: Props) => {
+	return (
+		<div className='winfo'>
+			<div className='winfo-title'>{white.title}</div>
+			<div className='winfo-author'>
+				<span className='winfo-author-label'>Author</span>
+				<span className='winfo-author-text'>{white.author}</span>
+			</div>
+			<div className='winfo-date'>
+				<span className='winfo-date-label'>whitelication</span>
+				<span className='winfo-date-text'>{white.date}</span>
+			</div>
+			<div className='winfo-more'>
+				<span className='winfo-more-text'>자세히 알아보기</span>
+				<img className='winfo-more-arrow' src={images.rightB} />
+			</div>
+		</div>
+	);
+};
+
+export default whitelicationInfo;
---
+++ src/store/zustand/whitePageZustand.js
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+import images from 'src/assets/images';
+const useWhitePageStore = create((set) => ({
+	currentPage: 1,
+	setCurrentPage: (page) => set((state) => ({ ...state, currentPage: page })),
+	totalPage: 1,
+	setTotalPage: (total) => set((state) => ({ ...state, totalPage: total })),
+	// currentPages: [],
+	// setCurrentPages: (pages) =>
+	// 	set((state) => ({ ...state, currentPages: pages })),
+	onMovePage: (type, startIndex, endIndex) => {
+		set((state) => {
+			let newPage;
+			if (type === 'next') {
+				newPage = endIndex + 1;
+			} else if (type === 'prev') {
+				newPage = startIndex - 1;
+			} else if (type === 'start') {
+				newPage = 1;
+			} else {
+				newPage = state.totalPage;
+			}
+			return { ...state, currentPage: newPage };
+		});
+	},
+	onNumberClick: (number) =>
+		set((state) => ({ ...state, currentPage: number })),
+}));
+
+export default useWhitePageStore;
