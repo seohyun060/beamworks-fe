c9797423588ddc6e38892b0b19e84d9d9d3587a4
Noah <seohyun060@naver.com>
2023-12-28 10:28:18 +0900

    이ㅁ시

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,59 +1,60 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-import TeamIntro from "./components/TeamIntro";
-import TeamGoal from "./components/TeamGoal";
-import TeamDoing from "./components/TeamDoing";
-import TeamOther from "./components/TeamOther";
-import Footer from "src/pages/Footer/Footer";
+import TeamIntro from './components/TeamIntro';
+import TeamGoal from './components/TeamGoal';
+import TeamDoing from './components/TeamDoing';
+import TeamOther from './components/TeamOther';
+import Footer from 'src/pages/Footer/Footer';
 
-import "./styles/team.css";
-import TeamList from "./TeamList.json";
+import './styles/team.css';
+import TeamList from './TeamList.json';
+import FooterContainer from 'src/pages/Footer/containers/FooterContainer';
 
 const Team = (props) => {
-  const { jobName } = props;
-  const teamDoingData = TeamList.data[jobName].teamDoing;
-  const [teamDoingSet, setTeamDoingSet] = useState([]);
+	const { jobName } = props;
+	const teamDoingData = TeamList.data[jobName].teamDoing;
+	const [teamDoingSet, setTeamDoingSet] = useState([]);
 
-  // TeamList 파일의 데이터를 이용해서 팀 업무소개 페이지 제작
-  // 한페이지에 업무가 2개씩 들어가기 때문에 묶어주는 작업
-  const makeTeamDoing = () => {
-    for (let i = 0; i < teamDoingData.length; i = i + 2) {
-      if (i + 1 === teamDoingData.length) {
-        setTeamDoingSet((prev) => [
-          ...prev,
-          <TeamDoing key={i} teamDoing1={teamDoingData[i]} />,
-        ]);
-      } else
-        setTeamDoingSet((prev) => [
-          ...prev,
-          <TeamDoing
-            key={i}
-            teamDoing1={teamDoingData[i]}
-            teamDoing2={teamDoingData[i + 1]}
-          />,
-        ]);
-      {
-      }
-    }
-  };
+	// TeamList 파일의 데이터를 이용해서 팀 업무소개 페이지 제작
+	// 한페이지에 업무가 2개씩 들어가기 때문에 묶어주는 작업
+	const makeTeamDoing = () => {
+		for (let i = 0; i < teamDoingData.length; i = i + 2) {
+			if (i + 1 === teamDoingData.length) {
+				setTeamDoingSet((prev) => [
+					...prev,
+					<TeamDoing key={i} teamDoing1={teamDoingData[i]} />,
+				]);
+			} else
+				setTeamDoingSet((prev) => [
+					...prev,
+					<TeamDoing
+						key={i}
+						teamDoing1={teamDoingData[i]}
+						teamDoing2={teamDoingData[i + 1]}
+					/>,
+				]);
+			{
+			}
+		}
+	};
 
-  useEffect(() => {
-    makeTeamDoing();
-  }, []);
+	useEffect(() => {
+		makeTeamDoing();
+	}, []);
 
-  return (
-    <div className="Team">
-      <Link to="/recruitment" state={{team: jobName}}>
-        <button className="ApplyButton">지원하기</button>
-      </Link>
-      <TeamIntro jobName={jobName} />
-      <TeamGoal jobName={jobName} />
-      {teamDoingSet}
-      <TeamOther jobName={jobName} />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='Team'>
+			<Link to='/recruitment' state={{ team: jobName }}>
+				<button className='ApplyButton'>지원하기</button>
+			</Link>
+			<TeamIntro jobName={jobName} />
+			<TeamGoal jobName={jobName} />
+			{teamDoingSet}
+			<TeamOther jobName={jobName} />
+			<FooterContainer />
+		</div>
+	);
 };
 
 export default Team;
